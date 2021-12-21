6967e079c42111fc7b3ec4fd497db2ec1575b724
vvipVikash <54550178+vvipVikash@users.noreply.github.com>
2021-12-21 17:49:13 +0530

    Update aadharBack.py

--- aadharBack.py
+++ aadharBack.py
@@ -1,3 +1,4 @@
+#Working for all type of aadhar card 
 import easyocr
 import cv2
 
@@ -8,49 +9,57 @@ class AadharIdBackReader(object):
         self.reader = easyocr.Reader(['en'], gpu=False)
         self.output = {}
         self.img = None
+        self.leftImage = None
+        self.rightImage = None
         self.pattern = 0
 
-    def begin(self, img, pattern):
+    def begin(self, img):
         self.output = {}
         self.img = img
         self.img = cv2.fastNlMeansDenoisingColored(self.img, None, 10, 10, 7, 15)
-        self.pattern = pattern
         self.cropImage()
         self.extractInformation()
         return self.output
 
     def cropImage(self):
-        print("Inside crop")
         height, width, _ = self.img.shape
         width_cutoff = (width // 2) + 5
-        if self.pattern == 1: 
-            self.img= self.img[:, :width_cutoff]
-        elif self.pattern ==2:
-            self.img = self.img[:, width_cutoff:]
+        self.leftImage= self.img[:, :width_cutoff]
+        self.rightImage = self.img[:, width_cutoff:]
 
     def extractInformation(self):
+        start = 0
         sentence = ""
         flag = 0
-        results = self.reader.readtext(self.img, detail=1, paragraph=False)  
-        e11 = cv2.getTickCount()
-        for (bbox, text, prob) in results:
-            if "address" in text.lower():
-                flag = 1
-                continue
-            if flag == 1:
-                sentence += " " + text
-                if len(text) == 6 and text.isalnum():
-                    flag = 0
-        a = sentence.split(",", 1)
-        try:
-            self.output["Father's Name"] = ((a[0].split(":"))[1]).strip()
-        except:
-            self.output["Father's Name"] = ((a[0].split(";"))[1]).strip()
+        for i in range(2):
+            if i == 0:
+                results = self.reader.readtext(self.leftImage, detail=1, paragraph=False) 
+            elif i == 1:
+                results = self.reader.readtext(self.rightImage, detail=1, paragraph=False) 
+            for (bbox, text, prob) in results:
+                result = text.lower().find('address')
+                if result == 0:
+                    start = 1
+                else:
+                    continue
+                if start == 1:
+                    for (bbox, text, prob) in results:
+                        if "address" in text.lower():
+                            flag = 1
+                            continue
+                        if flag == 1:
+                            sentence += " " + text
+                            if len(text) == 6 and text.isalnum():
+                                flag = 0
+                        a = sentence.split(",", 1)
+                    try:
+                        self.output["gname"] = ((a[0].split(":"))[1]).strip()
+                    except:
+                        self.output["gname"] = ((a[0].split(";"))[1]).strip()
 
-        self.output["Address"] = a[1].strip()
+                    self.output["address"] = a[1].strip()
         
-
-x = AadharIdBackReader()
-im = cv2.imread("images/AdharBackD.jpg")
-z = x.begin(im, 2)
-print(z)
+assigning = AadharIdBackReader()
+image = cv2.imread("path/image_name.jpg")
+result = assigning.begin(image)
+print(result)
